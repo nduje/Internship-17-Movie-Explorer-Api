232d8222fbe7eebc9a4205d233ddf5ea414b7629
nduje <dujerancic@gmail.com>
2026-03-12 20:42:34 +0100

    Generate NestJS modules and services for Prisma, Movie, Genre, and Favorite resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { MovieModule } from './movie/movie.module';
+import { GenreModule } from './genre/genre.module';
+import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, MovieModule, GenreModule, FavoriteModule],
   controllers: [AppController],
   providers: [AppService],
 })
