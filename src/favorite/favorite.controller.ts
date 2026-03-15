ab7c01551d21ba12c1e94dbceffa477976c4546b
nduje <dujerancic@gmail.com>
2026-03-15 13:39:12 +0100

    Enable CORS to allow frontend to fetch API data

--- src/favorite/favorite.controller.ts
+++ src/favorite/favorite.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete } from '@nestjs/common';
 import { FavoriteService } from './favorite.service';
 import { FavoriteDto } from './dto/favorite.dto';
 
