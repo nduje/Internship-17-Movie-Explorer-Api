7d6e4cc4210a41f9b78999a115aaaa8ff9c0e318
nduje <dujerancic@gmail.com>
2026-03-18 00:17:19 +0100

    Protect favorite routes with UserAuthGuard

--- src/favorite/favorite.controller.ts
+++ src/favorite/favorite.controller.ts
@@ -1,16 +1,19 @@
-import { Controller, Get, Post, Body, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, UseGuards } from '@nestjs/common';
 import { FavoriteService } from './favorite.service';
 import { FavoriteDto } from './dto/favorite.dto';
+import { UserAuthGuard } from 'src/user/user-auth.guard';
 
 @Controller('favorite')
 export class FavoriteController {
   constructor(private readonly favoriteService: FavoriteService) {}
 
+  @UseGuards(UserAuthGuard)
   @Post()
   create(@Body() createFavoriteDto: FavoriteDto) {
     return this.favoriteService.create(createFavoriteDto);
   }
 
+  @UseGuards(UserAuthGuard)
   @Delete()
   remove(@Body() removeFavoriteDto: FavoriteDto) {
     return this.favoriteService.remove(removeFavoriteDto);
