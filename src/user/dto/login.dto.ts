5c387c8616fda67c856f6a225935db8281bc930f
nduje <dujerancic@gmail.com>
2026-03-20 18:00:44 +0100

    Update LoginDto

--- src/user/dto/login.dto.ts
+++ src/user/dto/login.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty()
@@ -8,5 +8,6 @@ export class LoginDto {
 
   @ApiProperty()
   @IsString()
+  @MinLength(6)
   password: string;
 }
